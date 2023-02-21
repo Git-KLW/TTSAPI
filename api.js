d80a6a743af22114dcd17d6ed82730b71597f101
Git-KLW <124479692+Git-KLW@users.noreply.github.com>
2023-02-20 20:46:22 -0600

    Update api.js

--- api.js
+++ api.js
@@ -11,7 +11,6 @@ for (var i = 0; i < voices.length; i++) {
   }
 }
 
-// Create a new instance of SpeechSynthesisUtterance
 // Get the URL queries
 const queryParams = new URLSearchParams(window.location.search);
 
@@ -23,10 +22,23 @@ if (text) {
   // Create a new SpeechSynthesisUtterance object with the text
   const message = new SpeechSynthesisUtterance(text);
 
-  // Use the Web Speech API to speak the text
+  // Use the Web Speech API to generate the audio data
   window.speechSynthesis.speak(message);
+
+  // Wait for the audio to finish generating
+  message.onend = function() {
+    // Get the audio data as a Blob object
+    const blob = new Blob([new Uint8Array(message.audioBuffer)]);
+
+    // Create a new URL object for the Blob
+    const url = URL.createObjectURL(blob);
+
+    // Send the URL as the response
+    window.location.href = url;
+  }
 } else {
   // Handle the case when the "text" parameter is not present
   console.error("No 'text' parameter found in the URL queries");
 }
+
 }
