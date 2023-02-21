fb16869e199b18543201d8442e2c4687a5342469
Git-KLW <124479692+Git-KLW@users.noreply.github.com>
2023-02-20 22:05:18 -0600

    Update api.js

--- api.js
+++ api.js
@@ -1,15 +1,3 @@
-window.speechSynthesis.onvoiceschanged = function() {
-
-  // Get the value of the text input field
-  var voices = window.speechSynthesis.getVoices();
-// Find the voice you want to use
-var selectedVoice = null;
-for (var i = 0; i < voices.length; i++) {
-  if (voices[i].name === 'Microsoft Liam Online (Natural) - English (Canada)') {
-    selectedVoice = voices[i];
-    break;
-  }
-}
 
 // Get the URL queries
 const queryParams = new URLSearchParams(window.location.search);
@@ -25,19 +13,20 @@ if (text) {
   // Use the Web Speech API to generate the audio data
   window.speechSynthesis.speak(message);
 
-  // Wait for the audio to finish generating
-  message.onend = function() {
-    // Get the audio data as a Blob object
-    const blob = new Blob([new Uint8Array(message.audioBuffer)]);
+  // Wait for the audio data to be generated
+  window.speechSynthesis.onvoiceschanged = () => {
+    // Get the audio data as a blob
+    const audioBlob = new Blob([new Uint8Array(window.speechSynthesis.getVoices()[0].synthesize(message))], { type: 'audio/wav' });
 
-    // Create a new URL object for the Blob
-    const url = URL.createObjectURL(blob);
-console.log(blob)
-    // Send the URL as the response
-  }
+    // Create a new URL object for the blob
+    const audioUrl = URL.createObjectURL(audioBlob);
+
+    // Return the audio URL as the response
+    window.location.href = audioUrl;
+  };
 } else {
   // Handle the case when the "text" parameter is not present
   console.error("No 'text' parameter found in the URL queries");
 }
 
-}
+
