71b12d8a77460eef6ffb619a1e8d40983e1af989
Git-KLW <124479692+Git-KLW@users.noreply.github.com>
2023-02-20 19:47:10 -0600

    Update api.js

--- api.js
+++ api.js
@@ -11,34 +11,22 @@ for (var i = 0; i < voices.length; i++) {
   }
 }
 
-const text = request.body.text;
+// Create a new instance of SpeechSynthesisUtterance
+// Get the URL queries
+const queryParams = new URLSearchParams(window.location.search);
 
-// Create a new SpeechSynthesisUtterance object with the text
-const message = new SpeechSynthesisUtterance(text);
+// Extract the "text" query parameter
+const text = queryParams.get("text");
 
-// Set the audio output format to WAV
-message.voiceURI = 'native';
-message.lang = 'en-US';
-message.volume = 1;
-message.rate = 1;
-message.pitch = 1;
-message.text = text;
-message.outputFormat = 'audio/wav';
+// Check if the "text" parameter is present
+if (text) {
+  // Create a new SpeechSynthesisUtterance object with the text
+  const message = new SpeechSynthesisUtterance(text);
 
-// Use the Web Speech API to generate the audio file
-window.speechSynthesis.speak(message);
-
-// Wait for the audio to be generated
-message.addEventListener('end', () => {
-  // Get the audio data as a blob
-  window.speechSynthesis.cancel();
-  const audioData = new Blob([new Uint8Array(message.audioContent)], {type: 'audio/wav'});
-
-  // Return the audio data as the HTTP response
-  response.writeHead(200, {
-    'Content-Type': 'audio/wav',
-    'Content-Length': audioData.size
-  });
-  response.end(audioData);
-});
+  // Use the Web Speech API to speak the text
+  window.speechSynthesis.speak(message);
+} else {
+  // Handle the case when the "text" parameter is not present
+  console.error("No 'text' parameter found in the URL queries");
+}
 }
